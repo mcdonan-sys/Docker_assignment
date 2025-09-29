f3a8e86fb958d55a8b1f856c6080ed329821f383
mcdonan-sys <mcdonan@grace.edu>
2025-09-29 13:21:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const app = express();
+const api = require('./api');
+
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Docker Node.js App Running!</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
 
